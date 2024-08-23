96e733dc654e0c1933601bea9c2e94c92fd07681
trantorian <114066155+Trantorian1@users.noreply.github.com>
2024-08-23 23:33:27 +0200

    feat(node): started work on node

--- src/node.rs
+++ src/node.rs
@@ -1,16 +1,26 @@
-use std::collections::HashMap;
+use std::usize;
 
 use blake2::{Blake2b, Digest};
+use indexmap::IndexMap;
 use rand::Rng;
 
 pub type DATA = u8;
 pub type GUID = [DATA; 30];
 type Blake2b160 = Blake2b<blake2::digest::consts::U30>;
 
+pub enum ConnectionStep {
+    Done { data: Vec<DATA> },
+    Failed { id: GUID },
+    Seeking { id: GUID },
+}
+
+pub struct KBucket<const k: usize> {}
+
+#[derive(Clone)]
 pub struct Node {
     guid: GUID,
-    peers: Vec<Node>,
-    storage: HashMap<GUID, DATA>,
+    peers: IndexMap<GUID, Node>,
+    storage: IndexMap<GUID, DATA>,
 }
 
 impl Node {
@@ -25,8 +35,30 @@ impl Node {
 
         Self {
             guid,
-            peers: vec![],
-            storage: HashMap::default(),
+            peers: IndexMap::default(),
+            storage: IndexMap::default(),
+        }
+    }
+
+    pub fn new_with_peers(name: &str, peers: &[Node]) -> Self {
+        let mut rng = rand::thread_rng();
+        let salt: GUID = rng.gen();
+
+        let mut hasher = Blake2b160::new();
+        hasher.update(salt);
+        hasher.update(name.as_bytes());
+        let guid: GUID = hasher.finalize().into();
+
+        let peers = peers
+            .iter()
+            .cloned()
+            .map(|node| (node.guid, node))
+            .collect::<IndexMap<_, _>>();
+
+        Self {
+            guid,
+            peers,
+            storage: IndexMap::default(),
         }
     }
 
@@ -34,15 +66,23 @@ impl Node {
         self.guid
     }
 
-    fn peers(&self) -> &[Node] {
-        &self.peers
+    fn peers(&self) -> indexmap::map::Values<[u8; 30], Node> {
+        self.peers.values()
     }
 
-    fn relay(&self, data: &[DATA], id: GUID) -> Option<Vec<DATA>> {
+    fn query(&self, data: &[DATA], id: GUID) -> ConnectionStep {
+        todo!()
+    }
+
+    fn store(&self, data: &[DATA], id: GUID) -> ConnectionStep {
+        todo!()
+    }
+
+    fn find_next_peer(&self, id: GUID) -> ConnectionStep {
         todo!()
     }
 
     fn ping(&self, id: GUID) -> bool {
-        self.guid == id || self.storage.contains_key(&id)
+        todo!()
     }
 }
